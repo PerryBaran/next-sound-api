4896cf93eb25dcacf5d82130c9bde57b817ba503
Perry Baran <perrybaran@gmail.com>
2024-01-19 12:18:09 +0000

    created album and song routes and controller folders

---
+++ src/routes/album.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const albumController = require('../controllers/album');
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(
+    auth.authenticateToken,
+    albumController.create
+  )
+  .get(albumController.readAll);
+
+router
+  .route('/:albumId')
+  .get(albumController.readById)
+  .patch(
+    auth.authenticateToken,
+    albumController.patch
+  )
+  .delete(auth.authenticateToken, albumController.delete);
+
+module.exports = router;
---
+++ src/routes/song.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const songController = require('../controllers/song');
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(
+    auth.authenticateToken,
+    songController.create
+  )
+  .get(songController.readAll);
+
+router
+  .route('/:songId')
+  .get(songController.readById)
+  .patch(
+    auth.authenticateToken,
+    songController.patch
+  )
+  .delete(auth.authenticateToken, songController.delete);
+
+module.exports = router;
